9a97b4dcd324008ffa2c30ab45b44b4df2492ac3
CDTester <chris_a_day@hotmail.com>
2025-09-18 17:36:12 +0100

    Learnt file handling

--- src/main/java/trainingFileHandling/Files.java
+++ src/main/java/trainingFileHandling/Files.java
@@ -1,15 +1,16 @@
 package trainingFileHandling;
 
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
+import java.util.Scanner;
 
 public class Files {
+  public String filePath;
   public String name;
   public String fileName;
   public File file;
 
   public Files(String name) {
-    String filePath = "./src/main/resources/";
+    this.filePath = "./src/main/resources/";
     this.name = name;
     this.fileName = filePath.concat(name);
     this.file = new File(fileName);
@@ -58,8 +59,25 @@ public class Files {
     }
   }
 
+  public void getName() {
+    System.out.println("File name is '" + this.file.getName() + "'");
+  }
+
   public void getAbsolutePath() {
     System.out.println("Absolute path of the file '" + this.name + "' is: " + this.file.getAbsolutePath());
+  }
+
+  public void getFileSize() {
+    System.out.println("File size is '" + this.file.length() + "' Bytes.");
+  }
+
+  public void deleteFile() {
+    if (this.file.delete()) {
+      System.out.println("File '" + this.name + "' was deleted successfully.");
+    }
+    else {
+      System.out.println("Failed to delete the file '" + this.name + "'.");
+    }
   }
 
   public void writeToFile(String content) {
@@ -73,5 +91,109 @@ public class Files {
     }
   }
 
+  public void appendToFile(String content) {
+    try (FileWriter fileWriter = new FileWriter(this.file, true)) {
+      fileWriter.append(content);
+      System.out.println("Successfully appended to the file '" + this.name + "'");
+    }
+    catch (Exception e) {
+      System.out.println("An error occurred while appending to the file: " + e.getMessage());
+      e.printStackTrace();
+    }
+  }
 
+  public void readBufferedToFile() {
+    try (BufferedReader reader = new BufferedReader(new FileReader(this.file))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+      }
+      System.out.println("Successfully read the file '" + this.name + "'");
+    }
+    catch (Exception e) {
+      System.out.println("An error occurred while reading to the file: " + e.getMessage());
+      e.printStackTrace();
+    }
+  }
+
+  public void writeBufferedFile(String[] content, boolean append) {
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(this.file, append))) {
+      for (String line : content) {
+        bw.write(line);
+        bw.newLine();  // add line break
+      }
+      System.out.println("Successfully wrote to the file.");
+    }
+    catch (IOException e) {
+      System.out.println("Error writing file.");
+    }
+  }
+
+  public void readScannerFile() {
+    try (Scanner myReader = new Scanner(this.file)) {
+      String line;
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+    }
+    catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+
+  public void readFileInputStream() {
+    try (FileInputStream input = new FileInputStream(this.file)) {
+      int byteData;
+      while ((byteData = input.read()) != -1) {
+        System.out.print((char) byteData);
+      }
+      System.out.println();
+    }
+    catch (Exception e) {
+      System.out.println("Error: An I/O error occurred. " + e);
+    }
+  }
+
+  public void writeFileOutputStream(String content) {
+    try (FileOutputStream output = new FileOutputStream(this.file)) {
+      output.write(content.getBytes());
+      System.out.println("Successfully wrote to the file '" + this.name + "'");
+    }
+    catch (Exception e) {
+      System.out.println("An error occurred while writing to the file: " + e.getMessage());
+      e.printStackTrace();
+    }
+  }
+
+  public void writeFileOutputStream(String content, boolean append) {
+    try (FileOutputStream output = new FileOutputStream(this.file, append)) {
+      output.write(content.getBytes());
+      System.out.println("Successfully wrote to the file '" + this.name + "'");
+    }
+    catch (Exception e) {
+      System.out.println("An error occurred while writing to the file: " + e.getMessage());
+      e.printStackTrace();
+    }
+  }
+
+  public void copyFile() {
+    File destFile = new File(this.filePath + "copied_" + this.file.getName());
+    destFile.delete();
+
+    try (FileInputStream input = new FileInputStream(this.file);
+         FileOutputStream output = new FileOutputStream(destFile)) {
+
+      int bytesRead;
+      while ((bytesRead = input.read()) != -1) {
+        output.write(bytesRead);
+      }
+      System.out.println("File copied successfully to " + this.filePath);
+    }
+    catch (IOException e) {
+      System.out.println("An error occurred while copying the file: " + e.getMessage());
+      e.printStackTrace();
+    }
+  }
 }
--- src/main/java/trainingFileHandling/Main.java
+++ src/main/java/trainingFileHandling/Main.java
@@ -6,7 +6,7 @@ public class Main {
     System.out.println("FILE");
     Files myFile = new Files("myFile.txt");
 
-    System.out.println("\nexists(): Tests whether the file exists.");
+    System.out.println("exists(): Tests whether the file exists.");
     myFile.doesExist();
 
     System.out.println("\ncreateNewFile(): Creates an empty file.");
@@ -20,32 +20,85 @@ public class Main {
     myFile.isWriteable();
 
     System.out.println("\ngetName(): Returns the name of the file.");
+    myFile.getName();
 
     System.out.println("\ngetAbsolutePath(): Returns the absolute pathname of the file.");
     myFile.getAbsolutePath();
 
     System.out.println("\nlength(): Returns the size of the file in bytes.");
+    myFile.getFileSize();
 
     System.out.println("\nlist(): Returns an array of the files in the directory.");
 
     System.out.println("\nmkdir(): Creates a directory.");
 
-    separator();
-    System.out.println("FILE WRITER");
+    System.out.println("\ndelete(): deletes a file.");
+    Files anotherFile = new Files("anotherFile.txt");
+    anotherFile.createFile();
+    anotherFile.doesExist();
+    anotherFile.deleteFile();
+    anotherFile.doesExist();
 
-    System.out.println("\nwrite(): FileWriter's write() method is used to write data to the file. This will overwrite the existing content in the file.");
+
+    separator();
+    System.out.println("There are classes that can be used to write files in Java:");
+    System.out.println("FileWriter - easiest choice for basic text files.");
+    System.out.println("BufferedWriter - better for larger files, as it writes data in chunks and supports handy features");
+    System.out.println("FileOutputStream - best for writing binary data, such as images or videos.");
+
+    System.out.println("\nFILE WRITER");
+    System.out.println("write(): FileWriter's write() method is used to write data to the file. This will overwrite the existing content in the file.");
     myFile.writeToFile("Files in Java might be tricky, but it is fun enough!\n");
 
-    System.out.println("\nappend(): FileWriter's append() method is used to write data to the file. This will append to the existing content in the file.");
+    System.out.println("\nwrite(): When FileWriter(fileName, true) is constructed, the write() method will append to the existing content in the file.");
+    myFile.appendToFile("Let's add this line to the file.\n");
+
+    System.out.println("\nFILE OUTPUT STREAM");
+    System.out.println("This class is used for writing binary data (images, audio, PDFs) or when you need full control of raw bytes.");
+    System.out.println("write(): Writes bytes from the specified byte array to this file output stream. string is converted to bytes using getBytes() method.");
+    System.out.println("The bytes need to be converted to characters using (char)byteData.");
+    Files myOutputStreamFile = new Files("fileOutputStream.txt");
+    myOutputStreamFile.writeFileOutputStream("Hello World!\n");
+    System.out.println("\nwrite(): When FileOutputStream(fileName, true) is constructed, the write() method will append to the existing content in the file.");
+    myOutputStreamFile.writeFileOutputStream("Hello Moon!\n", true);
+
+    System.out.println("\nBUFFERED WRITER");
+    System.out.println("This class makes writing faster by using a buffered memory.");
+    System.out.println("write() and newLine(): The write method adds text, and you can use newLine to insert a line break");
+    Files myBufferedWriterFile = new Files("bufferedWriter.txt");
+    String[] contents = {"Hello World!", "This is a new line.", "This is another line."};
+    myBufferedWriterFile.writeBufferedFile(contents, false);
 
 
     separator();
-    System.out.println("FILE READER");
+    System.out.println("There are classes that can be used to read files in Java:");
+    System.out.println("Scanner - best for basic text and when you want to parse numbers or words easily.");
+    System.out.println("FileReader - This one is not so popular, because BufferedReader is usually preferred.");
+    System.out.println("BufferedReader - best for large text files, because it is faster and reads line by line");
+    System.out.println("FileInputStream - best for binary data (images, audio, PDFs) or when you need full control of raw bytes");
 
+    System.out.println("\nSCANNER");
+    System.out.println("nextLine(): Scanner's nextLine() method is used to read data from the file line by line. A while loop is used to read the whole file.");
+    Files myScannerFile = new Files("scanner.txt");
+    myScannerFile.readScannerFile();
+    myScannerFile.getFileSize();
 
-    separator();
-    System.out.println("\ndelete(): Deletes the file.");
+    System.out.println("\nFILE INPUT STREAM");
+    System.out.println("This class is used for reading binary data (images, audio, PDFs) or when you need full control of raw bytes.");
+    System.out.println("read(): Reads a byte of data from this input stream. Returns the next byte of data, or -1 if the end of the file is reached.");
+    System.out.println("The bytes need to be converted to characters using (char)byteData.");
+    Files myInputStreamFile = new Files("fileInputStream.txt");
+    myInputStreamFile.readFileInputStream();
 
+    System.out.println("\nUsing FileInputStream, we can copy a binary file, such as an image and write it to another file.");
+    Files imageFile = new Files("java.jpg");
+    imageFile.copyFile();
+
+    System.out.println("\nBUFFERED READER");
+    System.out.println("This class makes reading faster by using a buffered memory.");
+    System.out.println("readLine(): Reads text line by line. A while loop is used to read the whole file.");
+    Files myBufferedReaderFile = new Files("bufferedReader.txt");
+    myBufferedReaderFile.readBufferedToFile();
 
     separator();
 
